4a5974b3d1078dd4cb18c23915bb5bf16d75a399
ikari2k <pluta.m@gmail.com>
2024-02-27 11:23:49 +0100

    fixed tets

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -6,10 +6,10 @@ from playwright.sync_api import Page
 class MyTest(unittest.TestCase):
     @pytest.fixture(autouse=True)
     def setup(self, page: Page):
-        self.page = Page
+        self.page = page
 
     def test_foobar(self):
-        page.goto(self, "https://microsoft.com")
+        self.page.goto("https://microsoft.com")
         self.page.locator(
             '[aria-label="Shop Surface Pro 8 and Surface Pro Keyboard Bundle\\."]'
         ).click()
